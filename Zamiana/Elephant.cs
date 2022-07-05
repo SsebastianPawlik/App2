6747cba717faff5b251342f3245d1a750c42024c
SsebastianPawlik <pawlik.sebastian00@gmail.com>
2022-07-05 12:49:22 +0200

    Zamiana

---
+++ Zamiana/Elephant.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Zamiana
+{
+    internal class Elephant
+    {
+        public string Name;
+        public int EarSize;
+        public void WhoAmI()
+        {
+            MessageBox.Show("Moje uszy mają " + EarSize + " centymerów szerokości.", Name + " mówi...");
+        }
+
+        
+    }
+}
