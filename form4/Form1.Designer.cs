e54a271dad9e5f901aefa4b89bc9381a86a50698
SsebastianPawlik <pawlik.sebastian00@gmail.com>
2022-07-05 11:10:56 +0200

    Joe i Bob

---
+++ form4/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace form4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.joeCashLable = new System.Windows.Forms.Label();
+            this.bobCashLable = new System.Windows.Forms.Label();
+            this.bankCashLable = new System.Windows.Forms.Label();
+            this.reciveCash = new System.Windows.Forms.Button();
+            this.GiveCash = new System.Windows.Forms.Button();
+            this.joeGive = new System.Windows.Forms.Button();
+            this.bobGive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // joeCashLable
+            // 
+            this.joeCashLable.AutoSize = true;
+            this.joeCashLable.Location = new System.Drawing.Point(12, 9);
+            this.joeCashLable.Name = "joeCashLable";
+            this.joeCashLable.Size = new System.Drawing.Size(68, 13);
+            this.joeCashLable.TabIndex = 0;
+            this.joeCashLable.Text = "Joe ma 50 zł";
+            // 
+            // bobCashLable
+            // 
+            this.bobCashLable.AutoSize = true;
+            this.bobCashLable.Location = new System.Drawing.Point(12, 43);
+            this.bobCashLable.Name = "bobCashLable";
+            this.bobCashLable.Size = new System.Drawing.Size(76, 13);
+            this.bobCashLable.TabIndex = 1;
+            this.bobCashLable.Text = "Bob ma 100 zł";
+            // 
+            // bankCashLable
+            // 
+            this.bankCashLable.AutoSize = true;
+            this.bankCashLable.Location = new System.Drawing.Point(12, 76);
+            this.bankCashLable.Name = "bankCashLable";
+            this.bankCashLable.Size = new System.Drawing.Size(82, 13);
+            this.bankCashLable.TabIndex = 2;
+            this.bankCashLable.Text = "Bank ma 100 zł";
+            // 
+            // reciveCash
+            // 
+            this.reciveCash.Location = new System.Drawing.Point(12, 107);
+            this.reciveCash.Name = "reciveCash";
+            this.reciveCash.Size = new System.Drawing.Size(97, 23);
+            this.reciveCash.TabIndex = 3;
+            this.reciveCash.Text = "Daj 10 zł Joemu";
+            this.reciveCash.UseVisualStyleBackColor = true;
+            this.reciveCash.Click += new System.EventHandler(this.reciveCash_Click);
+            // 
+            // GiveCash
+            // 
+            this.GiveCash.Location = new System.Drawing.Point(115, 107);
+            this.GiveCash.Name = "GiveCash";
+            this.GiveCash.Size = new System.Drawing.Size(102, 23);
+            this.GiveCash.TabIndex = 4;
+            this.GiveCash.Text = "Weź 5 zł od Boba";
+            this.GiveCash.UseVisualStyleBackColor = true;
+            this.GiveCash.Click += new System.EventHandler(this.GiveCash_Click);
+            // 
+            // joeGive
+            // 
+            this.joeGive.Location = new System.Drawing.Point(12, 136);
+            this.joeGive.Name = "joeGive";
+            this.joeGive.Size = new System.Drawing.Size(97, 23);
+            this.joeGive.TabIndex = 5;
+            this.joeGive.Text = "Joe daje bobowi";
+            this.joeGive.UseVisualStyleBackColor = true;
+            this.joeGive.Click += new System.EventHandler(this.joeGive_Click);
+            // 
+            // bobGive
+            // 
+            this.bobGive.Location = new System.Drawing.Point(115, 136);
+            this.bobGive.Name = "bobGive";
+            this.bobGive.Size = new System.Drawing.Size(102, 23);
+            this.bobGive.TabIndex = 6;
+            this.bobGive.Text = "Bob daje joemu";
+            this.bobGive.UseVisualStyleBackColor = true;
+            this.bobGive.Click += new System.EventHandler(this.bobGive_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 181);
+            this.Controls.Add(this.bobGive);
+            this.Controls.Add(this.joeGive);
+            this.Controls.Add(this.GiveCash);
+            this.Controls.Add(this.reciveCash);
+            this.Controls.Add(this.bankCashLable);
+            this.Controls.Add(this.bobCashLable);
+            this.Controls.Add(this.joeCashLable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label joeCashLable;
+        private System.Windows.Forms.Label bobCashLable;
+        private System.Windows.Forms.Label bankCashLable;
+        private System.Windows.Forms.Button reciveCash;
+        private System.Windows.Forms.Button GiveCash;
+        private System.Windows.Forms.Button joeGive;
+        private System.Windows.Forms.Button bobGive;
+    }
+}
+
---
+++ form4/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form4
+{
+    public partial class Form1 : Form
+    {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+        public void UpdateForm()
+        {
+            joeCashLable.Text = joe.Name + " ma " + joe.Cash + " zł";
+            bobCashLable.Text = bob.Name + " ma " + bob.Cash + " zł";
+            bankCashLable.Text = "Bank ma " + bank + " zł";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            bob = new Guy() { Cash = 100, Name = "Bob" };
+         /*   bob.Name = "Bob";
+            bob.Cash = 100;*/
+
+            joe = new Guy() { Cash = 50, Name = "Joe" };
+      /*      joe.Name = "Joe";
+            joe.Cash = 50;*/
+
+            UpdateForm();
+        }
+
+        private void reciveCash_Click(object sender, EventArgs e)
+        {
+            if(bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Bank nie posiada kasy");
+            }
+        }
+
+        private void GiveCash_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
+        }
+
+        private void joeGive_Click(object sender, EventArgs e)
+        {
+            if(joe.Cash >= 10)
+            {
+                joe.Cash -= 10;
+                bob.Cash += 10;
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Joe biedak nie ma kasy");
+            }
+        }
+
+        private void bobGive_Click(object sender, EventArgs e)
+        {
+            if (bob.Cash >= 5)
+            {
+                bob.Cash -= 5;
+                joe.Cash += 5;
+                UpdateForm();
+            }
+            else
+            {
+                MessageBox.Show("Bob biedak nie ma kasy");
+            }
+        }
+    }
+}
