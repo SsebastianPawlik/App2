2d972ae116293d1bc1b36cbaca849550880f0d09
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-06 08:59:48 +0200

    Add files via upload

---
+++ Bar3/Bar3/MenuMaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bar3
+{
+    internal class MenuMaker
+    {
+            public Random Randomizer;
+
+            string[] Meats = { "Pieczona wołowina", "Salami", "Indyk", "Szynka", "Karkówka" };
+            string[] Condiments = {"Zółta musztarda", "brąowa musztarda", "musztarda miodowa",
+            "majonez", "przyprawa","sos francuski"};
+            string[] Bread = {"Chleb ryżowy", "Chleb biały", "Chleb zbożowy","pumpernikiel",
+            "Chleb włoski","bułka"};
+        
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiments = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Bread[Randomizer.Next(Bread.Length)];
+
+            return randomMeat + ", " + randomCondiments + ", " + randomBread;
+        }
+    }
+
+}
