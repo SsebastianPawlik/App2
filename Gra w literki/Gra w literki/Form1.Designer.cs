e6ee2da9a467186769d33b719cda902ce48d7f48
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-06 10:25:00 +0200

    Gra w Literki
    Gra w Literki

---
+++ Gra w literki/Gra w literki/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Gra_w_literki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.correctLable = new System.Windows.Forms.ToolStripStatusLabel();
+            this.missedLable = new System.Windows.Forms.ToolStripStatusLabel();
+            this.totalLable = new System.Windows.Forms.ToolStripStatusLabel();
+            this.accuracyLable = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.difficultyProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 113;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(28, 25, 28, 25);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(854, 136);
+            this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.correctLable,
+            this.missedLable,
+            this.totalLable,
+            this.accuracyLable,
+            this.toolStripStatusLabel1,
+            this.difficultyProgressBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 114);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(854, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // correctLable
+            // 
+            this.correctLable.Name = "correctLable";
+            this.correctLable.Size = new System.Drawing.Size(93, 17);
+            this.correctLable.Text = "Prawidłowych: 0";
+            // 
+            // missedLable
+            // 
+            this.missedLable.Name = "missedLable";
+            this.missedLable.Size = new System.Drawing.Size(58, 17);
+            this.missedLable.Text = "Błędów: 0";
+            // 
+            // totalLable
+            // 
+            this.totalLable.Name = "totalLable";
+            this.totalLable.Size = new System.Drawing.Size(73, 17);
+            this.totalLable.Text = "Wszyskich: 0";
+            // 
+            // accuracyLable
+            // 
+            this.accuracyLable.Name = "accuracyLable";
+            this.accuracyLable.Size = new System.Drawing.Size(91, 17);
+            this.accuracyLable.Text = "Dokładność: 0%";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(422, 17);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "Poziom trudności:";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // difficultyProgressBar
+            // 
+            this.difficultyProgressBar.Maximum = 701;
+            this.difficultyProgressBar.Name = "difficultyProgressBar";
+            this.difficultyProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(57F, 108F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 136);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(28, 25, 28, 25);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel correctLable;
+        private System.Windows.Forms.ToolStripStatusLabel missedLable;
+        private System.Windows.Forms.ToolStripStatusLabel totalLable;
+        private System.Windows.Forms.ToolStripStatusLabel accuracyLable;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar difficultyProgressBar;
+    }
+}
+
