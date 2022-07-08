eb2d5a50f9782a85e1565b1fb770e3007524676c
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-08 13:40:20 +0200

    KlejnotyAplikacjaKonsolowa
    KlejnotyAplikacjaKonsolowa

---
+++ KlejnotyAplikacjaKonsolowa/KlejnotyAplikacjaKonsolowa/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using KlejnotyAplikacjaKonsolowa;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Owner owner = new Owner();
+        Safe safe = new Safe();
+        JewelThief jewelThief = new JewelThief();
+        jewelThief.OpenSafe(safe, owner);
+        Console.ReadKey();
+    }
+}
---
+++ KlejnotyAplikacjaKonsolowa/KlejnotyAplikacjaKonsolowa/Safe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlejnotyAplikacjaKonsolowa
+{
+    internal class Safe
+    {
+        private Jewels contents = new Jewels();
+        private string safeCombination = "12345";
+        public Jewels Open(string combination)
+        {
+            if(combination == safeCombination)
+            {
+                return contents; 
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void PickLlock(Locksmith lockpicker)
+        {
+            lockpicker.WriteDownCombination(safeCombination);
+        }
+    }
+}
