ba04bf6ce3f82565a8480f46d2aff28838708bb1
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-08 10:21:58 +0200

    AplikacjaFarmera
    AplikacjaFarmera

---
+++ AplikacjaFarmera/AplikacjaFarmera/Farmer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikacjaFarmera
+{
+    internal class Farmer
+    {
+        public int WorkiKarmy { get; private set; }
+        private int karmienie;
+        public int Karmienie { get { return Karmienie; } }
+
+        private int iloscKrow;
+        public int IloscKrow
+        {
+            get
+            {
+                return iloscKrow;
+            }
+            set
+            {
+                iloscKrow = value;
+                WorkiKarmy = iloscKrow * Karmienie;
+            }
+        }
+        public Farmer(int iloscKrow, int karmienie)
+        {
+            this.karmienie = karmienie;
+            IloscKrow = iloscKrow;
+        }
+    }
+}
---
+++ AplikacjaFarmera/AplikacjaFarmera/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikacjaFarmera
+{
+    public partial class Form1 : Form
+    {
+        Farmer farmer;
+        public Form1()
+        {
+            InitializeComponent();
+            farmer = new Farmer() { IloscKrow = 15, karmienie = 30 };
+            farmer = new Farmer() { 15, 30 };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Potrzebne {0} worków do wykarmienia {1} krów", farmer.WorkiKarmy, farmer.IloscKrow);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            farmer.IloscKrow = (int)numericUpDown1.Value;
+        }
+    }
+}
