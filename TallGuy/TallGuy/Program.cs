3daa0d268ad0eff48bcfbc6608c88eb8f49dfd3e
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-11 13:38:21 +0200

    TallGuy
    TallGuy

---
+++ TallGuy/TallGuy/Program.cs
@@ -0,0 +1,29 @@
+﻿public class TallGuy
+{
+    public string Name;
+    public int Height;
+    public void TalkAboutYourself()
+    {
+        Console.WriteLine("Nazywam się " + Name + " i mam " + Height + " centymetrów wzrostu.");
+    }
+    static void Main(string[] args)
+    {
+        TallGuy tallGuy = new TallGuy() { Name = "Adam", Height = 189 };
+        tallGuy.TalkAboutYourself();
+        tallGuy.Honk();
+        
+    }
+    public interface IClown
+    {
+        string FunnyThingIHave { get;  }
+        void Honk();
+    }
+    public string FunnyThingIHave
+    {
+        get { return "Duże buty"; }
+    }
+    public void Honk()
+    {
+        Console.WriteLine("HOOOONK");
+    }
+}
