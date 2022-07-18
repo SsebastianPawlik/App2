4b7d1a16a2610b62bc0f494df747eb6f979bbd58
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-18 09:50:00 +0200

    KapitanKonsolaOdczyt
    KapitanKonsolaOdczyt

---
+++ KapitanKonsolowaOdczyt/KapitanKonsolowaOdczyt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+namespace KapitanKonsolowaOdczyt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StreamReader reader = new StreamReader(@"C:\Users\Praktykant\source\repos\LosowyZestawKartKonsolowa\TajnyPlik.txt");
+            StreamWriter writer = new StreamWriter(@"C:\Users\Praktykant\source\repos\KapitanKonsolowaOdczyt\emaildDoKapitana.txt");
+
+            writer.WriteLine("To: KapitanWspaniały");
+            writer.WriteLine("From: Komisarz");
+            writer.WriteLine("Subjec: Czy pomożesz ocalić świat?");
+            writer.WriteLine();
+            writer.WriteLine("Odkrliśmy plan kanciarza.");
+            while (!reader.EndOfStream)
+            {
+                string lineFromThePlan = reader.ReadLine();
+                writer.WriteLine("Plan ->" + lineFromThePlan);
+            }
+            writer.WriteLine();
+            writer.WriteLine("Możesz nam pomóc?");
+            writer.Close();
+            reader.Close();
+        }
+    }
+}
