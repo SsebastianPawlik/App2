08aa557c1d63c25b7b6c7c98458f8c2c9dff6e0c
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-18 13:39:25 +0200

    JoeIBobZapis
    JoeIBobZapis

---
+++ JoeIBobZapis/Guy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace form4
+{
+    [Serializable]
+    internal class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= Cash && amount > 0)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show("Nie ma wystarczającej ilości pieniędzy, aby oddać" + amount, Name + "powedział...");
+                return 0;
+            }
+        }
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                MessageBox.Show(amount + " to nie jest ilość pieniędzy jaką mogą wziąć ", Name + " powiedział...");
+                return 0;
+            }
+        }
+    }
+}
+
