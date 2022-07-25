3d3599c5a62ef42e12e018f77c38a05f2313363f
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-25 11:23:39 +0200

    JanekXaml
    JanekXaml

---
+++ JanekXAML/JanekXAML/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanekXAML
+{
+    internal class MenuItem
+    {
+        public string Meat { get; private set; }
+        public string Condiment { get; private set; }
+        public string Bread { get; private set; }
+
+        public MenuItem(string meat, string condiment, string bread)
+        {
+            Meat = meat;
+            Condiment = condiment;
+            Bread = bread;
+        }
+        public override string ToString()
+        {
+            return Meat + ", " + Condiment + ", " + Bread;
+        }
+    }
+}
---
+++ JanekXAML/JanekXAML/MenuMaker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+namespace JanekXAML
+{
+    internal class MenuMaker
+    {
+        private Random random = new Random();
+        private List<string> meats = new List<string>()
+        {"Pieczona wołowina","Salami","Indyk","Szynka","Karkówka"};
+        private List<string> condiments = new List<string>()
+        { "żółta musztarda","brązowa musztarda","musztarda miodowa","majonez","przyprawa","sos francuski"};
+        private List<string> breads = new List<string>()
+        { "chleb ryżowy","chleb biały","chleb zbożowy","pumpernikiel","chleb włoski","bułka"};
+        public ObservableCollection<MenuItem> Menu { get; private set; }
+        public DateTime GeneratedDate { get; private set; }
+        public int NumberOfItems { get; set; }
+        public MenuMaker()
+        {
+            Menu = new ObservableCollection<MenuItem>();
+            NumberOfItems = 10;
+            UpdateMenu();
+        }
+        private MenuItem CreateMenuItem()
+        {
+            string randomMeat = meats[random.Next(meats.Count)];
+            string randomCondiment = condiments[random.Next(condiments.Count)];
+            string randomBreads = breads[random.Next(breads.Count)];
+            return new MenuItem(randomMeat, randomCondiment, randomBreads);
+        }
+        public void UpdateMenu()
+        {
+            Menu.Clear();
+            for(int i = 0; i<NumberOfItems; i++)
+            {
+                Menu.Add(CreateMenuItem());
+            }
+            GeneratedDate = DateTime.Now;
+        }
+    }
+}
