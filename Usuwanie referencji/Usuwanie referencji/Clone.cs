d77af7a45250bdce7a20ab553de684286b01c6e9
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-27 09:37:35 +0200

    UsuwanieReferencji
    UsuwanieReferencji

---
+++ Usuwanie referencji/Usuwanie referencji/Clone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Usuwanie_referencji
+{
+    internal class Clone : IDisposable
+    {
+        public int Id { get; private set; }
+        public Clone(int Id)
+        {
+            this.Id = Id;
+        }
+        public void Dispose()
+        {
+            MessageBox.Show("Zostałem usunięty!", "Klon" + Id + ". mówi...");
+        }
+        ~Clone()
+        {
+            MessageBox.Show("Aaaa! Dopadłeś mnie!", "Klon" + Id + ". mówi...");
+        }
+    }
+}
