43a976fb17ee8365ca0168d6d4b7b83620b5568a
SsebastianPawlik <108515880+SsebastianPawlik@users.noreply.github.com>
2022-07-28 12:36:11 +0200

    BaseballXAML
    BaseballXAML

---
+++ BallXAML/BallXAML/Fan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace BallXAML
+{
+    internal class Fan
+    {
+        public ObservableCollection<string> FanSays = new ObservableCollection<string>();
+        private int pitcherNumber = 0;
+        public Fan(Ball ball)
+        {
+            ball.BallInPlay += new EventHandler(ball_BallInPlay);
+        }
+
+         void ball_BallInPlay(object sender, EventArgs e)
+        {
+            pitcherNumber++;
+            if(e is BallEventArgs)
+            {
+                BallEventArgs ballEventArgs = e as BallEventArgs;
+                if (ballEventArgs.Distance > 120 && ballEventArgs.Trajectory > 30)
+                    FanSays.Add("Rzut nr " + pitcherNumber + ": Home run! Idę po piłkę");
+                else FanSays.Add("Rzut nr " + pitcherNumber + ": Jeee! Do boju!");
+            }
+        }
+    }
+}
---
+++ BallXAML/BallXAML/Pitcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallXAML
+{
+    internal class Pitcher
+    {
+        public ObservableCollection<string> PitcherSays = new ObservableCollection<string>();
+        private int pitcherNumer = 0;
+        public Pitcher(Ball ball)
+        {
+            ball.BallInPlay += new EventHandler(ball_BallInPlay);
+        }
+
+        void ball_BallInPlay(object sender, EventArgs e)
+        {
+            if(e is BallEventArgs)
+            {
+                BallEventArgs ballEventArgs = e as BallEventArgs;
+                if ((ballEventArgs.Distance < 29) && (ballEventArgs.Trajectory < 600))
+                    CatchBall();
+                else
+                    CoverFristBase();
+            }
+        }
+
+        private void CoverFristBase()
+        {
+            PitcherSays.Add("Rzut nr " + pitcherNumer + ": Złapałem piłkę.");
+        }
+
+        private void CatchBall()
+        {
+            PitcherSays.Add("Rzut nr " + pitcherNumer + ": Pokryłem pierwszą bazę.");
+        }
+    }
+}
